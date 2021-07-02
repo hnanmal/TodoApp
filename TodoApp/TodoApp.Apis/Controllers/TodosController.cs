72b3f3c8a1b804c74c30103ab1b37e7bc7704973
pjmk007 <60164230+pjmk007@users.noreply.github.com>
2021-07-02 22:32:17 +0900

    web api

---
+++ TodoApp/TodoApp.Apis/Controllers/TodosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApp.Apis.Controllers
+{
+    public class TodosController : ControllerBase
+    {
+    }
+}
